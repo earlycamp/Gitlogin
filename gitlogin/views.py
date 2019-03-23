ed9cb365373a72df4ee23184b71bf9aac8df98ea
Blackadams <adamssicero@gmail.com>
2019-03-23 13:55:37 +0300

    templates updated

--- gitlogin/views.py
+++ gitlogin/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-
-# @login_required
 def land(request):
     return render(request, 'land.html')
-
-def login(request):
-    return render(request, 'login.html')
