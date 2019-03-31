72eb0abbfc52076ce4cd6c4cd2c197a07d1187a1
Blackadams <adamssicero@gmail.com>
2019-03-31 16:18:04 +0300

    routes edited

--- gitproject/urls.py
+++ gitproject/urls.py
@@ -51,7 +51,7 @@ urlpatterns = [
     path('accounts/login/', auth_views.LogoutView.as_view(template_name='logout.html'),name='logout2'),
 
     path('login/', auth_views.LoginView.as_view(template_name='login.html'),name='login'),
-    path('login/',auth_views.LogoutView.as_view(template_name='login.html'), name = 'login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='login.html'), name = 'logout'),
 
 
 ]
